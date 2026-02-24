4b122906b9a07b9cc764ff4b1812a2c04a935968
Andy Kirk <andy@andykirk.net>
2026-02-24 15:23:04 +0000

    v1.0.0

--- src/Extension/Blocks.php
+++ src/Extension/Blocks.php
@@ -11,16 +11,36 @@ namespace NPEU\Plugin\System\Blocks\Extension;
 
 defined('_JEXEC') or die;
 
+/*
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+*/
 use Joomla\CMS\Event\Result\ResultAwareInterface;
 use Joomla\CMS\Factory;
 use Joomla\CMS\Helper\ModuleHelper;
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Plugin\CMSPlugin;
 use Joomla\CMS\Session\Session;
+use Joomla\CMS\Table\Module as ModuleTable;
 use Joomla\Database\DatabaseInterface;
 use Joomla\Event\Event;
 use Joomla\Event\SubscriberInterface;
 use Joomla\Registry\Registry;
+
+
+use NPEU\Plugin\System\Blocks\Helper\BlocksHelper;
+
+
+use Joomla\CMS\Log\Log;
+
+
+Log::addLogger(
+    array('text_file' => 'debug-ajax.php'),
+    Log::ALL,
+    array('plg_blocks') // change to your component/plugin name
+);
+
 
 /**
  * Plugin for Blocks component.
@@ -56,6 +76,62 @@ class Blocks extends CMSPlugin implements SubscriberInterface
         parent::__construct($subject, $config);
     }
 
+
+    /**
+     * Resolve a SITE module layout path (works when running in administrator)
+     *
+     * @param  string  $moduleName  e.g. 'mod_custom' or 'mod_mything'
+     * @param  string  $layout      e.g. 'default'
+     * @return string|false         full path to layout file or false if not found
+     */
+    public function resolveSiteModuleLayoutPath(string $moduleName, string $layout = 'default')
+    {
+        // Normalize inputs
+        $moduleName = trim($moduleName);
+        $layout = trim($layout) ?: 'default';
+
+        // candidate 1: active site template override
+
+        $db = Factory::getContainer()->get(DatabaseInterface::class);
+
+        $query = $db->getQuery(true)
+            ->select($db->quoteName('template'))
+            ->from($db->quoteName('#__template_styles'))
+            ->where($db->quoteName('client_id') . ' = 0')
+            ->where($db->quoteName('home') . ' = 1')
+            ->order($db->quoteName('id') . ' DESC')
+            ->setLimit(1);
+
+        try {
+            $db->setQuery($query);
+            $template = $db->loadResult();
+        } catch (\Throwable $e) {
+            $template = null;
+        }
+        Log::add('template: ' . $template, \Joomla\CMS\Log\Log::INFO, 'plg_blocks');
+        if (!empty($template)) {
+            $tplOverride = JPATH_SITE . '/templates/' . $template . '/html/' . $moduleName . '/' . $layout . '.php';
+            if (is_file($tplOverride)) {
+                return $tplOverride;
+            }
+        }
+
+        // candidate 2: module's own tmpl folder
+        $moduleTmpl = JPATH_SITE . '/modules/' . $moduleName . '/tmpl/' . $layout . '.php';
+        if (is_file($moduleTmpl)) {
+            return $moduleTmpl;
+        }
+
+        // candidate 3: sometimes module uses layout files directly under module (older styles)
+        $moduleLayoutRoot = JPATH_SITE . '/modules/' . $moduleName . '/' . $layout . '.php';
+        if (is_file($moduleLayoutRoot)) {
+            return $moduleLayoutRoot;
+        }
+
+        // not found
+        return false;
+    }
+
     /**
      * function for getSubscribedEvents : new Joomla 4 feature
      *
@@ -68,6 +144,7 @@ class Blocks extends CMSPlugin implements SubscriberInterface
         return self::$enabled ? [
             'onAfterDispatch'      => 'onAfterDispatch',
             'onBeforeRender'       => 'onBeforeRender',
+            'onAfterRender'        => 'onAfterRender',
             'onAjaxGetModuleById'  => 'onAjaxGetModuleById',
             'onContentPrepareData' => 'onContentPrepareData'
         ] : [];
@@ -139,19 +216,30 @@ class Blocks extends CMSPlugin implements SubscriberInterface
      */
     public function onAjaxGetModuleById(Event $event): void
     {
-        //https://dev.npeu.ox.ac.uk/administrator/index.php?option=com_ajax&format=raw&plugin=getModuleById
+        //https://www.npeu.ox.ac.uk/administrator/index.php?option=com_ajax&format=raw&plugin=getModuleById
+
+        // Note: I've spent a long time on this, trying to get a standard Joomla approach to loading
+        // modules to work, but it won't. This is because we need to load and render a SITE module
+        // but we're in an ADMIN context, so the usual stuff doesn't work.
+        // So be aware that we HAVE to use the DB to get the module object, and AI helped to write
+        // the template layout path function to find the right template for it to render.
 
         $app = Factory::getApplication();
+
+        // Log application and user info
+        Log::add('AJAX called. App client: ' . $app->getName(), Log::INFO, 'plg_blocks');
+
         if (!$app->isClient('administrator')) {
             return; // Only run in admin
         }
         $user = $app->getSession()->get('user');
-        #echo '<pre>'; var_dump($user); echo '<pre>'; exit;
+
         if(!$user->authorise('core.create', 'com_blocks')) {
             return; // Only authorised users
         }
 
         $input     = $app->getInput();
+        $module    = false;
         $module_id = $input->get('module_id', false);
 
         if (!$module_id || !is_numeric($module_id)) {
@@ -162,50 +250,51 @@ class Blocks extends CMSPlugin implements SubscriberInterface
         $db = Factory::getContainer()->get(DatabaseInterface::class);
 
         $query = $db->getQuery(true)
-                ->select('*')
-                ->from($db->quoteName('#__modules'))
-                ->where($db->quoteName('id') . ' = ' . $module_id);
+            ->select('*')
+            ->from($db->quoteName('#__modules'))
+            ->where($db->quoteName('id') . ' = ' . $module_id);
         $db->setQuery($query);
 
-        $module_data = $db->loadObject();
-        $module_data->params = json_decode($module_data->params);
-
-        #$output = json_encode($module_data);
-        $output = $module_data;
-
-        #echo '<pre>'; var_dump($module_data); echo '<pre>'; exit;
-        #echo '<pre>'; var_dump($output); echo '<pre>'; exit;
-        /*
-        So, rendering normal templates here doesn't work because we're in admin (at least I can't
-        find a way).
-        ALso it might be quite liberating and flexible to allow each module to provide its own Twig
-        template (possibly hidden in admin config rather than user-specified, not sure).
-        So here we should either get that temolate and add to data or render it and return the
-        HTML output - I'm not sure at this stage. Probably render it and add the rendered output
-        to the data so we have both for whatever reason.
+        $module = $db->loadObject();
 
 
-        */
-        // This is temporary - we need to run this through the Twig template but that doesn't
-        // exist yet.
-        $output->rendered_output = $module_data->content;
+        $module->params = json_decode($module->params);
 
-        #$module = ModuleHelper::getModuleById((int) $module_id);
-        #echo '<pre>'; var_dump($module); echo '<pre>'; exit;
+        $layout = 'default';
 
-        #$module->displayFull = true;
-        #echo ModuleHelper::renderModule($module);
-        #echo HTMLHelper::_('content.prepare', '{loadmoduleid ' . $module_id . '}');
+        $layoutPath = $this->resolveSiteModuleLayoutPath($module->module, $layout);
+
+        if (!$layoutPath) {
+            Log::add('Module layout not found for ' . $module->module . ' layout ' . $layout, \Joomla\CMS\Log\Log::WARNING, 'plg_blocks');
+            return;
+        }
+
+        if (!is_file($layoutPath)) {
+            return;
+        }
+
+        $params  = new \Joomla\Registry\Registry($module->params ?? '');
+        $attribs = [];
+
+        ob_start();
+
+        try {
+            include $layoutPath;
+        } catch (\Throwable $e) {
+            ob_end_clean();
+            return;
+        }
+
+        $module->rendered_output = ob_get_clean();
 
 
 
-        #exit;
 
         if ($event instanceof ResultAwareInterface) {
-            $event->addResult($output);
+            $event->addResult($module);
         } else {
             $result = $event->getArgument('result') ?? [];
-            $result[] = $output;
+            $result[] = $module;
             $event->setArgument('result', $result);
         }
 
@@ -224,8 +313,8 @@ class Blocks extends CMSPlugin implements SubscriberInterface
         }
 
         $option = $app->input->get('option');
-        if ($option != 'com_menus') {
-            return; // Only run in com_menus
+        if (! ($option == 'com_menus' || $option == 'com_modules')) {
+            return; // Only run in com_menus and com_modules
         }
 
         $dir = str_replace(JPATH_ROOT, '', dirname(dirname(__DIR__)));
@@ -235,6 +324,46 @@ class Blocks extends CMSPlugin implements SubscriberInterface
 
         $document->addScript($dir . '/assets/js/blocks-admin.js');
     }
+
+
+        /**
+     * Add CSS and JS for admin.
+     */
+    public function onAfterRender(Event $event): void
+    {
+        $app = Factory::getApplication();
+
+        if (!$app->isClient('administrator')) {
+            return; // Only run in admin
+        }
+
+        $option = $app->input->get('option');
+        if (! ($option == 'com_menus' || $option == 'com_modules')) {
+            return; // Only run in com_menus and com_modules
+        }
+
+        $sprite = BlocksHelper::getSpriteHtml();
+        if ($sprite === '') {
+            return;
+        }
+
+        $body = $app->getBody();
+
+        // Avoid double injection
+        if (str_contains($body, 'id="' . BlocksHelper::getIdPrefix())) {
+            return;
+        }
+
+        $pos = strripos($body, '</body>');
+        if ($pos !== false) {
+            $body = substr_replace($body, $sprite . PHP_EOL, $pos, 0);
+        } else {
+            $body .= $sprite;
+        }
+
+        $app->setBody($body);
+    }
+
 
 
     /**
